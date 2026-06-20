c058e691e60a47b6dd0551bae6fd0eff6c5421e1
Christopher Homberger <christopher.homberger@web.de>
2026-06-20 19:48:01 +0200

    only buf reader

--- xodus/src/xvd/math.rs
+++ xodus/src/xvd/math.rs
@@ -31,7 +31,7 @@ pub fn page_number_to_offset(page_number: u64) -> u64 {
     page_number * PAGE_SIZE as u64
 }
 
-pub fn calculate_hash_block_num_and_run_for_block_num(
+pub fn calculate_hash_block_num_for_block_num(
     xvd_type: u32,
     hash_tree_levels: u64,
     number_of_hashed_pages: u64,
@@ -39,20 +39,19 @@ pub fn calculate_hash_block_num_and_run_for_block_num(
     hash_level: u32,
     resilient: bool,
     unknown: bool,
-) -> (u64, u64, u64) {
+) -> (u64, u64) {
     fn hash_block_exponent(block_count: u32) -> u64 {
         (PAGES_PER_BLOCK as u64).pow(block_count)
     }
 
     if xvd_type > 1 || hash_level > 3 {
-        return (0xFFFF, 0, 1);
+        return (0xFFFF, 0);
     }
 
     let entry_num_in_block = (block_num / hash_block_exponent(hash_level)) % PAGES_PER_BLOCK as u64;
-    let run_length = PAGES_PER_BLOCK as u64 - entry_num_in_block;
 
     if hash_level == 3 {
-        return (0, entry_num_in_block, run_length);
+        return (0, entry_num_in_block);
     }
 
     let mut result = block_num / hash_block_exponent(hash_level + 1);
@@ -80,7 +79,7 @@ pub fn calculate_hash_block_num_and_run_for_block_num(
         result += 1;
     }
 
-    (result, entry_num_in_block, run_length)
+    (result, entry_num_in_block)
 }
 
 pub fn calculate_number_of_hash_blocks_in_level(
